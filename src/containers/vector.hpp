b219ac6947f742ce349f35c03830563ab71b15dc
Runhard13 <r9375806222@gmail.com>
2022-02-09 16:29:59 +0000

    vector

--- src/containers/vector.hpp
+++ src/containers/vector.hpp
@@ -53,22 +53,61 @@ namespace ft
 			this->assign (first, last);
 		}
 
-		//copy constructor. Создает контейнер с копиями каждого элемента x
+		//copy constructor. Создает контейнер с копиями каждого элемента Х
 		vector(const vector& x) : _size(x._size), _capacity(x._capacity),
 								 _alloc(x._alloc)
 		{
 			_head = _alloc.allocate(_capacity);
 			size_type i = 0;
-			while (i < _size)
+			while (i++ < _size)
 				_alloc.construct(&_head[i], x._head[i]);
 		}
 
 		//деструктор
 		~vector()
 		{
-			for (iterator it = begin(); it != end(); ++it)
-				_alloc.destroy(&(*it));
-			_alloc.deallocate(_vector, _capacity);
+			size_type i = 0;
+			while (i++ < _size)
+				alloc.destroy(&_head[i]);
+			_alloc.deallocate(_head, _capacity);
 		}
+		//Оператор присваивания. копирует все элементы из Х в контейнер
+		vector& operator=(const vector& x)
+		{
+			vector copy(x);
+			swap(copy);
+			return (*this);
+		}
+
+		// Капасити
+
+		size_type size() const {return _size;}
+
+		//Потенциально максимально возможный размер, который может хранить контейнер. Зависит от ограничений системы или
+		//реализации. Аллоцирование такого объема памяти не гарантированно, мы просто обещаем попытаться.
+		size_type max_size() {return _alloc.max_size();} 
+
+		resize????
+
+		size_type capacity() const {return _capacity;}
+
+		//Проверка пустой ли контейнер или нет?
+		bool empty() const {return _size == 0;}
+
+		//Запрос на изменение капасити. Если запрашивается капасити больше, чем есть сейчас, то происходит реаллокация
+		//во всех остальных случаях не происходит ничего
+
+		void reserve(size_type n)
+			{
+				if (n > this->max_size())
+					throw std::length_error("resized above max_size");
+				if (n > _capacity)
+					reallocate(n);
+			}
+
+
+
+
+
 	};
 }
