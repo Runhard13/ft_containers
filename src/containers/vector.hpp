4d50e1d18ff4891ce080706affdeb95f1d1de6ab
Runhard13 <r9375806222@gmail.com>
2022-02-10 03:34:26 +0300

    still vector

--- src/containers/vector.hpp
+++ src/containers/vector.hpp
@@ -1,8 +1,10 @@
 #include <memory>
+#include <stdexcept>
+#include <cstddef>
 
 namespace ft
 {
-	template <class T, class Alloc = std::allocator<T>>
+	template <class T, class Alloc = std::allocator<T> >
 	class vector
 	{
 	public:
@@ -13,10 +15,7 @@ namespace ft
 		typedef typename allocator_type::pointer			pointer;
 		typedef typename allocator_type::const_pointer		const_pointer;
 
-		iterator?
-		const_iterator?
-		reverse_iterator?
-		const_reverse_iterator?
+		// TODO: iterators
 
 		typedef ptrdiff_t 	difference_type;
 		typedef size_t 		size_type;
@@ -30,12 +29,12 @@ namespace ft
 	public:
 		//дефолтный конструктор. Создает пустой контейнер без элементов
 		explicit vector (const allocator_type& alloc = allocator_type())
-				: _head(null_ptr), _size(0), _alloc(alloc), _capacity(0);
+				: _head(null), _size(0), _alloc(alloc), _capacity(0) {};
 
 		//fill constructor. Создает контейнер из n элементов. Каждый элемент это копия val
 		explicit vector (size_type n, const value_type& val = value_type(),
-						 const allocator_type& alloc = allocator_type())
-						 : _alloc(alloc), _size(n), _capacity(n)
+				   const allocator_type& alloc = allocator_type())
+				   : _alloc(alloc), _size(n), _capacity(n)
 		{
 			_head = _alloc.allocate(_capacity);
 			size_type i = 0;
@@ -55,7 +54,7 @@ namespace ft
 
 		//copy constructor. Создает контейнер с копиями каждого элемента Х
 		vector(const vector& x) : _size(x._size), _capacity(x._capacity),
-								 _alloc(x._alloc)
+								  _alloc(x._alloc)
 		{
 			_head = _alloc.allocate(_capacity);
 			size_type i = 0;
@@ -68,7 +67,7 @@ namespace ft
 		{
 			size_type i = 0;
 			while (i++ < _size)
-				alloc.destroy(&_head[i]);
+				_alloc.destroy(&_head[i]);
 			_alloc.deallocate(_head, _capacity);
 		}
 		//Оператор присваивания. копирует все элементы из Х в контейнер
@@ -79,15 +78,15 @@ namespace ft
 			return (*this);
 		}
 
-		// Капасити
+		// Капасити_____________________________________________________
 
 		size_type size() const {return _size;}
 
 		//Потенциально максимально возможный размер, который может хранить контейнер. Зависит от ограничений системы или
 		//реализации. Аллоцирование такого объема памяти не гарантированно, мы просто обещаем попытаться.
-		size_type max_size() {return _alloc.max_size();} 
+		size_type max_size() {return _alloc.max_size();}
 
-		resize????
+		//TODO: resize function
 
 		size_type capacity() const {return _capacity;}
 
@@ -98,16 +97,154 @@ namespace ft
 		//во всех остальных случаях не происходит ничего
 
 		void reserve(size_type n)
+		{
+			if (n > this->max_size())
+				throw std::length_error("resized above max_size");
+			if (n > _capacity)
+				reallocate(n);
+		}
+
+		//Элемент аксесс_________________________________________________
+		reference operator[] (size_type n) {return _vector[n];} // вызвать
+		// эту функцию с n вне границ контейнера - УБ
+		const_reference operator[] (size_type n) const {return _vector[n];}
+
+		//тоже самое, но бросает эксепшн в случае, если n находится вне
+		// границ контейнера
+		reference at(size_type n)
+		{
+			if (n >= _size)
+				throw std::out_of_range("out-of-range");
+			return (_head[n]);
+		}
+
+		const_reference at(size_type n) const
+		{
+			if (n >= _size)
+				throw std::out_of_range("out-of-range");
+			return (_head[n]);
+		}
+
+		//модиферс___________________________________________-
+
+		reference front() {return _head[0];}
+		const_reference front() const {return _head[0];}
+		reference back() {return _head[_size - 1];}
+		const_reference back() const {return _head[_size - 1];}
+
+		//assign range - элементы контейнера создаются из диапазона [first, last]
+		template <class InputIterator>
+		void assign (InputIterator first, InputIterator last)
+		{
+			// сначала ищем n, затем создаем контейнер с нужным количеством памяти, чтобы при каждом push_back по
+			// новой не аллоцировать память и не копировать туда данные
+			difference_type n = 0;
+			InputIterator count(first);
+			while (count != last)
 			{
-				if (n > this->max_size())
-					throw std::length_error("resized above max_size");
-				if (n > _capacity)
-					reallocate(n);
+				++count;
+				++n;
 			}
+			this->clear();
+			this->reserve(n);
+			while (first != last)
+			{
+				push_back(*first);
+				first++;
+			}
+		}
+
+		//assign fill - создает n элементов со значением val
+		void assign(size_type n, const value_type& val)
+		{
+			this->clear();
+			this->reserve(n);
+			size_type i = 0;
+			while (i++ < n)
+				this->push_back(val);
+		}
+
+		void push_back(const value_type& val)
+		{
+			if (_size >=_capacity)
+				reallocate(capacity_extension(_size + 1));
+			_alloc.construct(&_head[_size++], val);
+		}
+
+		void pop_back() { _alloc.destroy(&_head[--_size]);}
+
+		//single element insert - Вставляем элемент перед элементом на указанной позиции. Реаллокация должна
+		// происходить только в том случае, если новый размер контейнера превышает капасити.
+		iterator insert(iterator position, const value_type& val)
+		{
+			if (_size + 1 > _capacity)
+				reallocate(capacity_extension(_size + 1));
+			difference_type shift = (position - this->begin());
+			if (_size + 1 > _capacity)
+				reallocate(capacity_extension(_size + 1));
+			this->insert(iterator(this->begin() + shift), 1, val);
+			return (iterator(this->begin() + shift));
+		}
+
+		// fill insert - тоже самое что и single insert, но вставляем не один, а n элементов
+		void insert(iterator position, size_type n, const value_type& val)
+		{
+			if (n == 0)
+				return;
+			if (_size + n > _capacity)
+				reallocate(capacity_extension(_size + n));
+			difference_type shift = position - this->begin();
+			difference_type tmp = this->end() - this->begin();
+
+			this->resize(this->_size + n);
+			iterator end = this->end();
+			position = this->begin() + shift;
+			iterator tmp_end = this->begin() + tmp;
+			while (tmp_end != position)
+			{
+				--end;
+				--tmp_end;
+				*end = *tmp_end;
+			}
+			while (n--)
+				*position++ = val;
+		}
+
+		void clear()
+		{
+			size_type i = 0;
+			while (i++ < _size)
+				_alloc.destroy(&_head[i]);
+			_size = 0;
+		}
 
 
 
+	private:
+		size_type capacity_extension(size_type new_size)
+		{
+			size_type		multiplier = 2;
 
+			size_type	new_capacity = (_capacity > 0 ? _capacity : 1);
+			while (new_capacity < new_size)
+				new_capacity *= multiplier;
+			return (new_capacity);
+		}
+
+		void reallocate(size_type new_capacity)
+		{
+			pointer new_vector = _alloc.allocate(new_capacity);
+			size_type i = 0;
+			while (i++ < _size)
+			{
+				_alloc.construct(&new_vector[i], _head[i]);
+				_alloc.destroy(&_head[i]);
+			}
+			if (_capacity)
+				_alloc.deallocate(_head, _capacity);
+			_head = new_vector;
+			_capacity = new_capacity;
+		}
 
 	};
 }
---
+++ test/test.cpp
@@ -0,0 +1,13 @@
+#include "../src/containers/vector.hpp"
+
+int main ()
+{
+	ft::vector<int> ft_vector;
+
+	ft_vector.pop_back();
+	ft_vector.push_back(5);
+	ft_vector.push_back(ft_vector4.front());
+	ft_vector.resize(7,2);
+
+	return (0);
+}
