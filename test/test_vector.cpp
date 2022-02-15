c52cf3cecf3a092d6cd5ba2f4cdbc75b00ab03aa
Runhard13 <r9375806222@gmail.com>
2022-02-15 03:58:08 +0300

    fefef

--- test/test_vector.cpp
+++ test/test_vector.cpp
@@ -3,14 +3,15 @@
 #include <iostream>
 #include <iomanip>
 
-template <class T>
-std::string is_equal(const T& t1, const T& t2) { return t1 == t2 ? "ok" : "not ok"; }
+template<class T>
+std::string
+is_equal(const T &t1, const T &t2) { return t1 == t2 ? "ok" : "not ok"; }
 
 template<class T>
 std::string
 is_equal_content(
-	 std::vector<T> &stl_vector,
-	 ft::vector<T> &ft_vector
+	std::vector<T> &stl_vector,
+	ft::vector<T> &ft_vector
 )
 {
 	typename ft::vector<T>::iterator ft_it;
@@ -30,10 +31,11 @@ is_equal_content(
 	return ("EQUAL");
 }
 
-template <class T>
-void printValues(
-	std::vector<T>& stl_vector,
-	ft::vector<T>& ft_vector, std::string testName
+template<class T>
+void
+printValues(
+	std::vector<T> &stl_vector,
+	ft::vector<T> &ft_vector, std::string testName
 )
 {
 	//real vector
@@ -54,8 +56,6 @@ void printValues(
 	std::string max_size = is_equal(ft_max_size, stl_max_size);
 	std::string capacity = is_equal(ft_capacity, stl_capacity); // в текущей реализации не всегда равны эти параметры
 	std::string content = is_equal_content(stl_vector, ft_vector);
-
-
 
 	std::cout << std::setfill('*') << std::setw(70) << std::left << testName << std::endl;
 	std::cout << std::setfill(' ') << std::setw(20) << std::left << "Attribute";
@@ -92,10 +92,11 @@ void printValues(
 	std::cout << "**********************************************************************" << std::endl;
 }
 
-template <class T>
-void printVectors(
-	std::vector<T>& stl_vector,
-	ft::vector<T>& ft_vector
+template<class T>
+void
+printVectors(
+	std::vector<T> &stl_vector,
+	ft::vector<T> &ft_vector
 )
 {
 	typename std::vector<T>::iterator stl_it;
@@ -133,264 +134,267 @@ test_vector()
 {
 	std::string testName;
 
-{	testName = "DEFAULT INT VECTOR";
+	{
+		testName = "DEFAULT INT VECTOR";
 
-	std::vector<int> stl_vector_default_int;
-	ft::vector<int> ft_vector_default_int;
+		std::vector<int> stl_vector_default_int;
+		ft::vector<int> ft_vector_default_int;
 
-	printValues(stl_vector_default_int, ft_vector_default_int, testName);
-	printVectors(stl_vector_default_int, ft_vector_default_int);
-}
+		printValues(stl_vector_default_int, ft_vector_default_int, testName);
+		printVectors(stl_vector_default_int, ft_vector_default_int);
+	}
 
-{
-	testName = "DEFAULT STRING VECTOR";
+	{
+		testName = "DEFAULT STRING VECTOR";
 
-	std::vector<std::string> stl_vector_default_string;
-	ft::vector<std::string> ft_vector_default_string;
+		std::vector<std::string> stl_vector_default_string;
+		ft::vector<std::string> ft_vector_default_string;
 
-	printValues(stl_vector_default_string, ft_vector_default_string, testName);
-	printVectors(stl_vector_default_string, ft_vector_default_string);
+		printValues(stl_vector_default_string, ft_vector_default_string, testName);
+		printVectors(stl_vector_default_string, ft_vector_default_string);
 
-}
+	}
 
-{
-	testName = "FILL CONSTRUCTOR";
+	{
+		testName = "FILL CONSTRUCTOR";
 
-	std::vector<int> stl_fill_vector(11);
-	ft::vector<int> ft_fill_vector(11);
+		std::vector<int> stl_fill_vector(11);
+		ft::vector<int> ft_fill_vector(11);
 
-	printValues(stl_fill_vector, ft_fill_vector, testName);
-	printVectors(stl_fill_vector, ft_fill_vector);
+		printValues(stl_fill_vector, ft_fill_vector, testName);
+		printVectors(stl_fill_vector, ft_fill_vector);
 
-	/* TODO: assign - функция неправильно работает. Надо пофиксить
-	testName = "FILL CONSTRUCTOR WITH VALUE";
+		testName = "FILL CONSTRUCTOR WITH VALUE";
 
-	std::vector<int> stl_fill_vector_with_value(11, 22);
-	ft::vector<int> ft_fill_vector_with_value(11, 22);
+		std::vector<int> stl_fill_vector_with_value(11, 22);
+		ft::vector<int> ft_fill_vector_with_value(11, 22);
 
-	printValues(stl_fill_vector_with_value, ft_fill_vector_with_value, testName);
-	printVectors(stl_fill_vector_with_value, ft_fill_vector_with_value);
-	*/
-}
+		printValues(stl_fill_vector_with_value, ft_fill_vector_with_value, testName);
+		printVectors(stl_fill_vector_with_value, ft_fill_vector_with_value);
+	}
 
-{
-	testName = "RANGE CONSTRUCTOR";
-	int array[] = { 44, 11, 89, 33, 55, 0 };
+	{
+		testName = "RANGE CONSTRUCTOR";
+		int array[] = {44, 11, 89, 33, 55, 0};
 
-	std::vector<int>::iterator stl_iterator(&(array[0]));
-	ft::vector<int>::iterator ft_iterator(&(array[0]));
+		std::vector<int>::iterator stl_iterator(&(array[0]));
+		ft::vector<int>::iterator ft_iterator(&(array[0]));
 
-	std::vector<int> stl_range_vector(stl_iterator, stl_iterator + 6);
-    ft::vector<int> ft_range_vector(ft_iterator, ft_iterator + 6);
+		std::vector<int> stl_range_vector(stl_iterator, stl_iterator + 6);
+		ft::vector<int> ft_range_vector(ft_iterator, ft_iterator + 6);
 
-	printValues(stl_range_vector, ft_range_vector, testName);
-	printVectors(stl_range_vector, ft_range_vector);
-}
+		printValues(stl_range_vector, ft_range_vector, testName);
+		printVectors(stl_range_vector, ft_range_vector);
+	}
 
-{
-	/* TODO: ABORTED (CORE DUMPED) - нужно пофиксить
-	testName = "COPY CONSTRUCTOR";
+	{
+		/* TODO: ABORTED (CORE DUMPED) - нужно пофиксить
+		testName = "COPY CONSTRUCTOR";
 
-    std::vector<int> stl_copy_vector(stl_range_vector);
-    ft::vector<int> ft_copy_vector(ft_range_vector);
+		std::vector<int> stl_copy_vector(stl_range_vector);
+		ft::vector<int> ft_copy_vector(ft_range_vector);
 
-	printValues(stl_copy_vector, ft_copy_vector, testName);
-	printVectors(stl_copy_vector, ft_copy_vector);
-	*/
-}
+		printValues(stl_copy_vector, ft_copy_vector, testName);
+		printVectors(stl_copy_vector, ft_copy_vector);
+		*/
+	}
 
-{
-	/*	TODO: ABORTED (CORE DUMPED) - нужно пофиксить
-	testName = "COPY CONSTRUCTOR";
-	int range_array[] = { -8, 1, 5, 5, 5333, 6323 };
+	{
+		/*	TODO: ABORTED (CORE DUMPED) - нужно пофиксить
+		testName = "COPY CONSTRUCTOR";
+		int range_array[] = { -8, 1, 5, 5, 5333, 6323 };
 
-	std::vector<int>::iterator stl_iterator(&(range_array[0]));
-    ft::vector<int>::iterator ft_iterator(&(range_array[0]));
+		std::vector<int>::iterator stl_iterator(&(range_array[0]));
+		ft::vector<int>::iterator ft_iterator(&(range_array[0]));
 
-    std::vector<int> stl_range_vector(stl_iterator, stl_iterator + 6);
-    ft::vector<int> ft_range_vector(ft_iterator, ft_iterator + 6);
+		std::vector<int> stl_range_vector(stl_iterator, stl_iterator + 6);
+		ft::vector<int> ft_range_vector(ft_iterator, ft_iterator + 6);
 
-    std::vector<int> stl_assign_vector = stl_range_vector;
-    ft::vector<int> ft_assign_vector = ft_range_vector;
+		std::vector<int> stl_assign_vector = stl_range_vector;
+		ft::vector<int> ft_assign_vector = ft_range_vector;
 
-	printValues(stl_assign_vector, ft_assign_vector, testName);
-	printVectors(stl_assign_vector, ft_assign_vector);
-	*/
-}
+		printValues(stl_assign_vector, ft_assign_vector, testName);
+		printVectors(stl_assign_vector, ft_assign_vector);
+		*/
+	}
 
-{
-	testName = "ITER BEGIN() and END()";
-	int range_array[] = { 187, 2, -54, 33, 5 };
+	{
+		testName = "ITER BEGIN() and END()";
+		int range_array[] = {187, 2, -54, 33, 5};
 
-    std::vector<int>::iterator stl_iterator(&(range_array[0]));
-    ft::vector<int>::iterator ft_iterator(&(range_array[0]));
+		std::vector<int>::iterator stl_iterator(&(range_array[0]));
+		ft::vector<int>::iterator ft_iterator(&(range_array[0]));
 
-    std::vector<int> stl_range_vector(stl_iterator, stl_iterator + 5);
-    ft::vector<int> ft_range_vector(ft_iterator, ft_iterator + 5);
-       
-    int from_stl = *(stl_range_vector.begin());
-    int from_ft = *(ft_range_vector.begin());
-	int from_stl_end = *(stl_range_vector.end() - 1);
-    int from_ft_end = *(ft_range_vector.end() - 1);
+		std::vector<int> stl_range_vector(stl_iterator, stl_iterator + 5);
+		ft::vector<int> ft_range_vector(ft_iterator, ft_iterator + 5);
 
-	printValues(stl_range_vector, ft_range_vector, testName);
-	std::cout << ".begin() from STL — " << from_stl << std::endl;
-	std::cout << ".end() from STL — " << from_stl_end << std::endl;
-	std::cout << "\n";
-	std::cout << ".begin() from FT — " << from_ft << std::endl;
-	std::cout << ".end() from FT — " << from_ft_end << std::endl;
-	std::cout << "\n";
-	printVectors(stl_range_vector, ft_range_vector);
+		int from_stl = *(stl_range_vector.begin());
+		int from_ft = *(ft_range_vector.begin());
+		int from_stl_end = *(stl_range_vector.end() - 1);
+		int from_ft_end = *(ft_range_vector.end() - 1);
 
-}
+		printValues(stl_range_vector, ft_range_vector, testName);
+		std::cout << ".begin() from STL — " << from_stl << std::endl;
+		std::cout << ".end() from STL — " << from_stl_end << std::endl;
+		std::cout << "\n";
+		std::cout << ".begin() from FT — " << from_ft << std::endl;
+		std::cout << ".end() from FT — " << from_ft_end << std::endl;
+		std::cout << "\n";
+		printVectors(stl_range_vector, ft_range_vector);
 
-{
-	testName = "CONST ITER BEGIN() and END()";
-	int range_array[] = { 94, 342, -2354, 33, 84 };
+	}
 
-    std::vector<int>::const_iterator stl_iterator(&(range_array[0]));
-    ft::vector<int>::const_iterator ft_iterator(&(range_array[0]));
+	{
+		testName = "CONST ITER BEGIN() and END()";
+		int range_array[] = {94, 342, -2354, 33, 84};
 
-    std::vector<int> stl_range_vector(stl_iterator, stl_iterator + 5);
-    ft::vector<int> ft_range_vector(ft_iterator, ft_iterator + 5);
-       
-    int from_stl = *(stl_range_vector.begin());
-    int from_ft = *(ft_range_vector.begin());
-	int from_stl_end = *(stl_range_vector.end() - 1);
-    int from_ft_end = *(ft_range_vector.end() - 1);
+		std::vector<int>::const_iterator stl_iterator(&(range_array[0]));
+		ft::vector<int>::const_iterator ft_iterator(&(range_array[0]));
 
-	printValues(stl_range_vector, ft_range_vector, testName);
-	std::cout << ".const_begin() from STL — " << from_stl << std::endl;
-	std::cout << ".const_end() from STL — " << from_stl_end << std::endl;
-	std::cout << "\n";
-	std::cout << ".const_begin() from FT — " << from_ft << std::endl;
-	std::cout << ".const_end() from FT — " << from_ft_end << std::endl;
-	std::cout << "\n";
-	printVectors(stl_range_vector, ft_range_vector);
+		std::vector<int> stl_range_vector(stl_iterator, stl_iterator + 5);
+		ft::vector<int> ft_range_vector(ft_iterator, ft_iterator + 5);
 
-}
+		int from_stl = *(stl_range_vector.begin());
+		int from_ft = *(ft_range_vector.begin());
+		int from_stl_end = *(stl_range_vector.end() - 1);
+		int from_ft_end = *(ft_range_vector.end() - 1);
 
-{
-	//TODO: reverse итератор не работает -___-
-	testName = "REVERSE ITER BEGIN() and END()";
-	int range_array[] = { 1, 2, -3, 4, 5 };
+		printValues(stl_range_vector, ft_range_vector, testName);
+		std::cout << ".const_begin() from STL — " << from_stl << std::endl;
+		std::cout << ".const_end() from STL — " << from_stl_end << std::endl;
+		std::cout << "\n";
+		std::cout << ".const_begin() from FT — " << from_ft << std::endl;
+		std::cout << ".const_end() from FT — " << from_ft_end << std::endl;
+		std::cout << "\n";
+		printVectors(stl_range_vector, ft_range_vector);
 
-    std::vector<int>::iterator stl_iterator(&(range_array[0]));
-    ft::vector<int>::iterator ft_iterator(&(range_array[0]));
+	}
 
-    std::vector<int> stl_range_vector(stl_iterator, stl_iterator + 5);
-    ft::vector<int> ft_range_vector(ft_iterator, ft_iterator + 5);
-       
-    int from_stl = *(stl_range_vector.rbegin());
-    int from_ft = *(ft_range_vector.rbegin());
-	int from_stl_end = *(stl_range_vector.rend() - 1);
-    int from_ft_end = *(ft_range_vector.rend() - 1);
+	{
+		//TODO: reverse итератор не работает -___-
+		testName = "REVERSE ITER BEGIN() and END()";
+		int range_array[] = {1, 2, -3, 4, 5};
 
-	printValues(stl_range_vector, ft_range_vector, testName);
-	std::cout << ".rbegin() from STL — " << from_stl << std::endl;
-	std::cout << ".rend() from STL — " << from_stl_end << std::endl;
-	std::cout << "\n";
-	std::cout << ".rbegin() from FT — " << from_ft << std::endl;
-	std::cout << ".rend() from FT — " << from_ft_end << std::endl;
-	std::cout << "\n";
-	printVectors(stl_range_vector, ft_range_vector);
+		std::vector<int>::iterator stl_iterator(&(range_array[0]));
+		ft::vector<int>::iterator ft_iterator(&(range_array[0]));
 
-}
-{
-	//TODO: reverse итератор не работает -___-
-	testName = "CONST REVERSE ITER BEGIN() and END()";
-	int range_array[] = { 1, 2, -3, 4, 5 };
+		std::vector<int> stl_range_vector(stl_iterator, stl_iterator + 5);
+		ft::vector<int> ft_range_vector(ft_iterator, ft_iterator + 5);
 
-    std::vector<int>::const_iterator stl_iterator(&(range_array[0]));
-    ft::vector<int>::const_iterator ft_iterator(&(range_array[0]));
+		int from_stl = *(stl_range_vector.rbegin());
+		int from_ft = *(ft_range_vector.rbegin());
+		int from_stl_end = *(stl_range_vector.rend() - 1);
+		int from_ft_end = *(ft_range_vector.rend() - 1);
 
-    std::vector<int> stl_range_vector(stl_iterator, stl_iterator + 5);
-    ft::vector<int> ft_range_vector(ft_iterator, ft_iterator + 5);
-       
-    int from_stl = *(stl_range_vector.rbegin());
-    int from_ft = *(ft_range_vector.rbegin());
-	int from_stl_end = *(stl_range_vector.rend() - 1);
-    int from_ft_end = *(ft_range_vector.rend() - 1);
+		printValues(stl_range_vector, ft_range_vector, testName);
+		std::cout << ".rbegin() from STL — " << from_stl << std::endl;
+		std::cout << ".rend() from STL — " << from_stl_end << std::endl;
+		std::cout << "\n";
+		std::cout << ".rbegin() from FT — " << from_ft << std::endl;
+		std::cout << ".rend() from FT — " << from_ft_end << std::endl;
+		std::cout << "\n";
+		printVectors(stl_range_vector, ft_range_vector);
 
-	printValues(stl_range_vector, ft_range_vector, testName);
-	std::cout << ".const_rbegin() from STL — " << from_stl << std::endl;
-	std::cout << ".const_rend() from STL — " << from_stl_end << std::endl;
-	std::cout << "\n";
-	std::cout << ".const_rbegin() from FT — " << from_ft << std::endl;
-	std::cout << ".const_rend() from FT — " << from_ft_end << std::endl;
-	std::cout << "\n";
-	printVectors(stl_range_vector, ft_range_vector);
-}
+	}
+	{
+		//TODO: reverse итератор не работает -___-
+		testName = "CONST REVERSE ITER BEGIN() and END()";
+		int range_array[] = {1, 2, -3, 4, 5};
 
-{
-	testName = "SIZE MANITULATIONS";
-	std::vector<int> stl_vector(1);
-	ft::vector<int> ft_vector(1);
+		std::vector<int>::const_iterator stl_iterator(&(range_array[0]));
+		ft::vector<int>::const_iterator ft_iterator(&(range_array[0]));
 
-	stl_vector.resize(2);
-	ft_vector.resize(2);
+		std::vector<int> stl_range_vector(stl_iterator, stl_iterator + 5);
+		ft::vector<int> ft_range_vector(ft_iterator, ft_iterator + 5);
 
-	stl_vector.push_back(11);
-	ft_vector.push_back(11);
-	stl_vector.push_back(14444444);
-	ft_vector.push_back(14444444);
+		int from_stl = *(stl_range_vector.rbegin());
+		int from_ft = *(ft_range_vector.rbegin());
+		int from_stl_end = *(stl_range_vector.rend() - 1);
+		int from_ft_end = *(ft_range_vector.rend() - 1);
 
-	stl_vector.reserve(1);
-	ft_vector.reserve(1);
+		printValues(stl_range_vector, ft_range_vector, testName);
+		std::cout << ".const_rbegin() from STL — " << from_stl << std::endl;
+		std::cout << ".const_rend() from STL — " << from_stl_end << std::endl;
+		std::cout << "\n";
+		std::cout << ".const_rbegin() from FT — " << from_ft << std::endl;
+		std::cout << ".const_rend() from FT — " << from_ft_end << std::endl;
+		std::cout << "\n";
+		printVectors(stl_range_vector, ft_range_vector);
+	}
 
-	stl_vector.push_back(11);
-	ft_vector.push_back(11);
-	stl_vector.push_back(11);
-	ft_vector.push_back(11);
-	stl_vector.push_back(11);
-	ft_vector.push_back(11);
+	{
+		testName = "SIZE MANITULATIONS";
+		std::vector<int> stl_vector(1);
+		ft::vector<int> ft_vector(1);
 
-	stl_vector.reserve(100);
-    ft_vector.reserve(100);
-        
-    stl_vector.push_back(345);
-    ft_vector.push_back(345);
+		stl_vector.resize(2);
+		ft_vector.resize(2);
 
-	printValues(stl_vector, ft_vector, testName);
-	printVectors(stl_vector, ft_vector);
-}
+		stl_vector.push_back(11);
+		ft_vector.push_back(11);
+		stl_vector.push_back(14444444);
+		ft_vector.push_back(14444444);
 
+		stl_vector.reserve(1);
+		ft_vector.reserve(1);
+
+		stl_vector.push_back(11);
+		ft_vector.push_back(11);
+		stl_vector.push_back(11);
+		ft_vector.push_back(11);
+		stl_vector.push_back(11);
+		ft_vector.push_back(11);
+
+		stl_vector.reserve(100);
+		ft_vector.reserve(100);
+
+		stl_vector.push_back(345);
+		ft_vector.push_back(345);
+
+		printValues(stl_vector, ft_vector, testName);
+		printVectors(stl_vector, ft_vector);
+	}
+	{
+		testName = "OPERATOR[]";
+
+		std::vector<int> stl_vector(10);
+		ft::vector<int> ft_vector(10);
+
+		unsigned i = 0;
+		while (i++ < 4)
+			stl_vector[i] = i;
+
+		i = 0;
+		while (i++ < 4)
+			ft_vector[i] = i;
+
+		printValues(stl_vector, ft_vector, testName);
+		printVectors(stl_vector, ft_vector);
+	}
+
+/* TODO: double free need to be fixed
 {
 	testName = "OPERATOR[]";
 
 	std::vector<int> stl_vector(10);
-	ft::vector<int> ft_vector(10);
-	std::vector<int>::size_type sz_stl = stl_vector.size();
-	std::vector<int>::size_type sz_ft = ft_vector.size();
-	
+	ft::vector<int> ft_vector(00);
+
 	unsigned i = 0;
-	while (i++ < sz_stl)
+	while (i++ < 10)
 		stl_vector[i] = i;
 
 	i = 0;
-	while (i++ < sz_stl/2)
-	{
-		int tmp;
-		tmp = stl_vector[sz_stl - 1 - i];
-		stl_vector[sz_stl - 1 - i] = stl_vector[i];
-		stl_vector[i] = tmp;
-	}		
-	
-	i = 0;
-	while (i++ < sz_ft)
+
+	while (i++ < 10)
 		ft_vector[i] = i;
 
-	i = 0;
-	while (i++ < sz_ft)
-	{
-		int tmp;
-		tmp = ft_vector[sz_ft - 1 - i];
-		ft_vector[sz_ft - 1 - i] = ft_vector[i];
-		ft_vector[i] = tmp;
-	}		
+	const int stl_const = stl_vector[1];
+	const int ft_const = ft_vector[1];
 
-	printValues(stl_vector, ft_vector, testName);
-	printVectors(stl_vector, ft_vector);
+	printValues(stl_const, ft_const, testName);
+	printVectors(stl_const, ft_const);
   }
+*/
+
 
 }
